71f4d21fdee0b0d16ed70d04e5987f393ccfbee7
Krzysztof Rzymkowski <krzysztof.rzymkowski@pragmatists.pl>
2019-09-06 17:08:32 +0200

    small

--- src/@types/accepts.d.ts
+++ src/@types/accepts.d.ts
@@ -1,34 +1,11 @@
-/// <reference types="node" />
 
 declare module "accepts" {
-    import * as React from 'react';
 
     namespace ReactPDF {
         export interface Style {}
-        interface Styles {
-            [key: string]: Style;
-        }
-
 
         const StyleSheet: {
-            hairlineWidth: number;
-            create: (styles: Styles) => Styles;
-            resolve: (
-                style: Style,
-                container: {
-                    width: number;
-                    height: number;
-                    orientation: Orientation;
-                },
-            ) => Style;
-            flatten: (...style: (Style[] | Style | undefined)[]) => Style;
-            absoluteFillObject: {
-                position: 'absolute';
-                left: 0;
-                right: 0;
-                top: 0;
-                bottom: 0;
-            };
+            create: Style;
         };
 
     }
